fab59c5a8a4a60011fc8cc08f19edaaab3dfafda
mark <plamagnum@gmail.com>
2024-12-15 13:48:20 +0200

    xml bug

--- newac/index.php
+++ newac/index.php
@@ -21,11 +21,8 @@
         <div class="right-column">
             <h2>XML File Content</h2>
             <?php
-            if (isset($_GET['success']) && $_GET['success'] == 1 && file_exists('uploaded.xml')) {
-                $xml = simplexml_load_file('uploaded.xml');
-                echo '<pre>';
-                print_r($xml);
-                echo '</pre>';
+            if (isset($_GET['success']) && $_GET['success'] == 1 && file_exists('results.txt')) {
+                echo nl2br(file_get_contents('results.txt'));
             }
             ?>
         </div>
--- newac/process.php
+++ newac/process.php
@@ -9,4 +9,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['xmlfile'])) {
         echo 'File upload failed!';
     }
 }
+
+if (isset($_GET['success']) && $_GET['success'] == 1 && file_exists('uploaded.xml')) {
+    $xml = simplexml_load_file('uploaded.xml');
+    $results = [];
+
+    foreach ($xml->host->ports->port as $port) {
+        $portid = (string)$port['portid'];
+        $protocol = (string)$port['protocol'];
+        $state = (string)$port->state['state'];
+        $service = (string)$port->service['name'];
+        $product = (string)$port->service['product'];
+        $results[] = "$portid/$protocol  $state   $service     $product";
+    }
+
+    file_put_contents('results.txt', implode("\n", $results));
+}
 ?>
